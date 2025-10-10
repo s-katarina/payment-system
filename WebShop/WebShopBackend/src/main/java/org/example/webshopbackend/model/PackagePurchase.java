95608b500526e6e174da97b606b8079b832c628b
Katarina S. <spremic.katarina@gmail.com>
2025-10-10 22:29:07 +0200

    Fix Purchase entity and Add dockerfile

--- WebShop/WebShopBackend/src/main/java/org/example/webshopbackend/model/PackagePurchase.java
+++ WebShop/WebShopBackend/src/main/java/org/example/webshopbackend/model/PackagePurchase.java
@@ -1,6 +1,7 @@
 package org.example.webshopbackend.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class PackagePurchase {
 
     @Column
